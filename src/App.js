bcf569a6c66e331d47c9952c10a1cd165a9a5a40
carlovenus <carlo.venusino@gmail.com>
2024-10-21 16:00:12 +0200

    RxJS + React playground

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ajax } from 'rxjs/ajax';
+import { map } from 'rxjs/operators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { data: [] };
+        this.subscription = null;  // Initialize the subscription as null
+    }
+
+    componentDidMount() {
+        const response = ajax('https://jsonplaceholder.typicode.com/users').pipe(
+            map(e => e.response)
+        );
+
+        // Subscribe to the response and set state when data is received
+        this.subscription = response.subscribe(res => {
+            this.setState({ data: res });
+        });
+    }
+
+    componentWillUnmount() {
+        // Unsubscribe from the observable if the component is unmounted
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Using RxJS with ReactJS</h3>
+                <ul>
+                    {this.state.data.map(el => (
+                        <li key={el.id}>
+                            {el.id}: {el.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
